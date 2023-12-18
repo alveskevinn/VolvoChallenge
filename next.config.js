e498dd08f0eef2116ce56ea6c343d29a912d6f6d
Kevin Pedroso <kevin.alves13@hotmail.com>
2023-12-18 07:51:39 -0300

    add: create next.config file

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// next.config.js
+
+module.exports = {
+    async headers() {
+      return [
+        {
+          source: '/api/:path*',
+          headers: [
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+          ],
+        },
+      ];
+    },
+  };
+  
